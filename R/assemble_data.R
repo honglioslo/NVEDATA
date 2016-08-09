249ba986eb3b4b42871aa24755c3d3908bb8256c
fbaffie <fbaffie@gmail.com>
2016-08-09 13:20:34 +0200

    fixed pb with hbv_2016

--- R/assemble_data.R
+++ R/assemble_data.R
@@ -370,7 +370,7 @@ load_flood_data <- function(regine_main = meta_data$regine_main) {
   HBV_2014 <- tidyr::gather(HBV_2014, key = Tmp, value = Values, -time, -regine.main, -station.name, -nbname) %>%
     tidyr::separate(Tmp, into = c("Type", "Variable"), sep = "_")
 
-  HBV_2016 <- dplyr::right_join(HBV_2016_INIT, HBV_2016_PRECIP_CORRECTION, by = c("regine.main", "time"))
+  HBV_2016 <- dplyr::right_join(HBV_2016_INIT, HBV_2016_PRECIP_CORRECTION, by = c("regine.main", "time", "nbname"))
   HBV_2016 <- tidyr::gather(HBV_2016, key = Tmp, value = Values, -time, -regine.main, -station.name, -nbname) %>%
     tidyr::separate(Tmp, into = c("Type", "Variable"), sep = "_")
 
